30a120b4a85e0ea4dc946d1127e15c311e471479
Mushkir <mushkirmohamed@gmail.com>
2024-09-05 23:09:17 +0530

    feat: implemented find total amount of cart items feature

--- resources/views/home/view_cart_items.blade.php
+++ resources/views/home/view_cart_items.blade.php
@@ -1,28 +1,35 @@
 @extends('layouts.home_layout')
 @section('title', auth()->user()->name . '\'s Cart items')
 @section('main-content')
-    <div class="p-5">
+    <div class="p-5 table-responsive">
         <table class="table table-bordered text-center">
-            <thead>
-                <th>S.No</th>
-                <th>Product Title</th>
-                <th>Image</th>
-                <th>Price</th>
-                <th>Action</th>
+            <thead class="bg-dark text-light ">
+                <th class="align-middle">S.No</th>
+                <th class="align-middle">Product Title</th>
+                <th class="align-middle">Image</th>
+                <th class="align-middle">Price</th>
+                <th class="align-middle">Action</th>
             </thead>
-
+            <?php
+            $totalAmount = 0;
+            ?>
             <tbody>
                 @foreach ($cartItems as $cart)
                     <tr>
-                        <td>#{{ $no++ }}</td>
-                        <td> {{ $cart->product->title }}</td>
-                        <td><img src="{{ asset('product/' . $cart->product->image) }}" class="w-25 h-25"
-                                alt="{{ $cart->product->title }}'s image"></td>
-                        <td>${{ $cart->product->price }}</td>
-                        <td><a href="{{ url('remove_cart_item', $cart->id) }}" class="btn btn-danger">Remove</a></td>
+                        <td class="align-middle">#{{ $no++ }}</td>
+                        <td class="align-middle"> {{ $cart->product->title }}</td>
+                        <td class="align-middle"><img src="{{ asset('product/' . $cart->product->image) }}"
+                                class="w-25 h-25" alt="{{ $cart->product->title }}'s image"></td>
+                        <td class="align-middle">${{ $cart->product->price }}</td>
+                        <td class="align-middle"><a href="{{ url('remove_cart_item', $cart->id) }}"
+                                class="btn btn-danger">Remove</a></td>
                     </tr>
+                    <?php
+                    $totalAmount += $cart->product->price;
+                    ?>
                 @endforeach
             </tbody>
         </table>
+        <h3 class="text-center mt-5">Your total amount is <span class="text-danger">${{ $totalAmount }}</span></h3>
     </div>
 @endsection
